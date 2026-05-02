c046029170cd55356f2ad767e907fa83043993cb
Brendan Holly <bholly24@gmail.com>
2026-05-02 07:23:02 -0400

    Initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,19 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import rehypeMermaid from 'rehype-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://sitecore.brendanholly.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		syntaxHighlight: {
+			type: 'shiki',
+			excludeLangs: ['mermaid', 'math'],
+		},
+		rehypePlugins: [[rehypeMermaid, { strategy: "img-svg", dark: true, theme: 'dark' }]],
+	},
 	fonts: [
 		{
 			provider: fontProviders.local(),
