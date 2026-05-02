642e02f948c0d2cfcd7c7b5e2666f86b7794bebf
Brendan Holly <bholly24@gmail.com>
2026-05-02 08:35:41 -0400

    Tooling (#2)
    * Move to pnpm.
    
    * Add in nvmrc and npmrc as best practice
    
    * Run biome and setup scripts
    
    * Add in a basic CI step.
    
    * Add playwright cache so that CI runs benefit from teh cache

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,42 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
-import rehypeMermaid from 'rehype-mermaid';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
+import rehypeMermaid from "rehype-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sitecore.brendanholly.com',
+	site: "https://sitecore.brendanholly.com",
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		syntaxHighlight: {
-			type: 'shiki',
-			excludeLangs: ['mermaid', 'math'],
+			type: "shiki",
+			excludeLangs: ["mermaid", "math"],
 		},
-		rehypePlugins: [[rehypeMermaid, { strategy: "img-svg", dark: true, theme: 'dark' }]],
+		rehypePlugins: [
+			[rehypeMermaid, { strategy: "img-svg", dark: true, theme: "dark" }],
+		],
 	},
 	fonts: [
 		{
 			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
+			name: "Atkinson",
+			cssVariable: "--font-atkinson",
+			fallbacks: ["sans-serif"],
 			options: {
 				variants: [
 					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
+						src: ["./src/assets/fonts/atkinson-regular.woff"],
 						weight: 400,
-						style: 'normal',
-						display: 'swap',
+						style: "normal",
+						display: "swap",
 					},
 					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
+						src: ["./src/assets/fonts/atkinson-bold.woff"],
 						weight: 700,
-						style: 'normal',
-						display: 'swap',
+						style: "normal",
+						display: "swap",
 					},
 				],
 			},
